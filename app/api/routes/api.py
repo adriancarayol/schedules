53a815af74ceb584c25c3995b8d9b07aa1162491
Adrian Carayol Orenes <adriancarayol@gmail.com>
2021-06-28 00:02:42 +0200

    Added logger / isort with black profile

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from api.routes import healthcheck
+from api.routes import healthcheck, schedules
 
 router = APIRouter()
 router.include_router(healthcheck.router, tags=["healthcheck"], prefix="/healthcheck")
+router.include_router(schedules.router, tags=["schedules"], prefix="/schedules")
